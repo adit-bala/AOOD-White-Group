429ae182a04ccc8456a0271182670b7bbe3deb24
Adit Bala <adityarbala@gmail.com>
2020-11-19 11:20:38 -0500

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,7 +1,7 @@
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("test 2");
+		System.out.println("test");
 		System.out.println("Matthew Won is done");
 		System.out.println("Aditya Bala is done");
 	}
