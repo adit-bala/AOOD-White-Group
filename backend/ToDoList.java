ea2141a2fb985502479536e6bcaaaa2bac16b694
aechen2330 <74388890+aechen2330@users.noreply.github.com>
2020-12-03 10:54:13 -0500

    Update ToDoList.java
    I have thwarted tyler's sabotage.

--- backend/ToDoList.java
+++ backend/ToDoList.java
@@ -1,11 +1,10 @@
 package backend;
-
 import java.time.*;
 import java.util.*;
 
 public class ToDoList {
-	private List<ActionItem> incompleteItems;
-	private List<ActionItem> completeItems;
+	private List<ActionItem> incompleteItems = new ArrayList<ActionItem>();
+	private List<ActionItem> completeItems = new ArrayList<ActionItem>();
 
 	public List<ActionItem> getIncompleteItems() {
 		List<ActionItem> incompleteItems = new ArrayList<ActionItem>();
@@ -25,7 +24,8 @@ public class ToDoList {
 
 	public void moveActionItem(int oldIndex, int newIndex) {
 		ActionItem temp = incompleteItems.get(oldIndex);
-		incompleteItems.add(newIndex, temp);
+		incompleteItems.remove(temp);
+		incompleteItems.add(newIndex,temp);
 		if (incompleteItems.get(newIndex).getPriority() != incompleteItems.get(newIndex - 1).getPriority())
 			incompleteItems.get(newIndex).setPriority(incompleteItems.get(newIndex - 1).getPriority());
 	}
@@ -38,7 +38,32 @@ public class ToDoList {
 	public void updateActionItem(int index, String title, Priority priority, LocalDate urgentDate,
 			LocalDate currentDate, LocalDate eventualDate, String comment) {
 		ActionItem item = incompleteItems.get(index);
-		//incomplete
+		if (!title.equals(item.getTitle())){
+			item.addEventToHistory(new TitleChangeEvent(LocalDate.now(),item.getTitle(),title));
+			item.setTitle(title);
+		}
+		if (priority!=item.getPriority()) {
+			item.addEventToHistory(new PriorityChangeEvent(LocalDate.now(),item.getPriority(),priority));
+			item.setPriority(priority);
+		}
+		if (!urgentDate.equals(item.getUrgentByDate())) {
+			item.setUrgentByDate(urgentDate);
+		}
+		if (!currentDate.equals(item.getCurrentByDate())) {
+			item.setCurrentByDate(currentDate);
+		}
+		if (!eventualDate.equals(item.getEventualByDate())) {
+			item.setEventualByDate(eventualDate);
+		}
+		if (!comment.equals(item.getComment())) {
+			int type = CommentChangeEvent.EDIT;
+			if(comment.equals(""))
+				type = CommentChangeEvent.DELETE;
+			else if (item.getComment().equals(""))
+				type = CommentChangeEvent.ADD;
+			item.addEventToHistory(new CommentChangeEvent(LocalDate.now(),type,comment));
+			item.setComment(comment);
+		}
 	}
 
 	public void deleteActionItem(int index) {
@@ -46,6 +71,6 @@ public class ToDoList {
 	}
 
 	public void addActionItem(ActionItem item) {
-		incompleteItems.add(0, item);
+		incompleteItems.add(0,item);
 	}
 }
