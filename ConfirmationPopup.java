47b546cf30be6002f1d5046fd15a9118403a0f63
cwang0114 <71450396+cwang0114@users.noreply.github.com>
2020-12-03 23:05:03 -0500

    ConfirmationPopup and MenuBar

---
+++ ConfirmationPopup.java
@@ -0,0 +1,5 @@
+
+
+public class ConfirmationPopup extends JDialog {
+
+}
--- MenuBar.java
+++ MenuBar.java
@@ -1,12 +1,58 @@
-import java.awt.Event;
-import java.awt.Font;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.util.List;
-
 import javax.swing.*;
 
-public class MenuBar extends JMenuBar{
+import backend.FontLoader;
+
+import java.awt.GridBagLayout;
+import java.awt.Color;
+import java.awt.GridBagConstraints;
+import java.awt.event.*;
+
+public class MenuBar extends JMenuBar implements ActionListener{
 	private JMenu file; 
+	private JMenuItem createBackup, restoreBackup, printList;
+	private JButton completedActionItems, quit;
+	private JFileChooser fileChooser; 
+	private JFrame mainScreenFrame;
 	
+	final static Color BAR_COLOR = Color.decode("#56997F");
+	
+	MenuBar(JPanel mainScreen){
+		this.setBackground(BAR_COLOR);
+		file = new JMenu("File");
+		file.setFont(FontLoader.loadFont("src/fonts/Chivo-Regular.ttf", 15));
+		file.setBackground(BAR_COLOR);
+		file.setForeground(Color.white);
+		createBackup = new JMenuItem("Create Backup");
+		createBackup.setFont(FontLoader.loadFont("src/fonts/Chivo-Regular.ttf", 12));
+		restoreBackup = new JMenuItem("Restore Backup...");
+		restoreBackup.setFont(FontLoader.loadFont("src/fonts/Chivo-Regular.ttf", 12));
+		printList = new JMenuItem("Print List");
+		printList.setFont(FontLoader.loadFont("src/fonts/Chivo-Regular.ttf", 12));
+		file.add(createBackup);
+		file.add(restoreBackup);
+		file.add(printList);
+		
+		completedActionItems = new JButton("Completed Action Items");
+		completedActionItems.setFont(FontLoader.loadFont("src/fonts/Chivo-Regular.ttf", 15));
+		completedActionItems.setBackground(BAR_COLOR);
+		quit = new JButton("Quit");
+		quit.setFont(FontLoader.loadFont("src/fonts/Chivo-Regular.ttf", 10));
+		quit.setBackground(BAR_COLOR);
+
+		mainScreen.add(file);
+		mainScreen.add(completedActionItems);
+		mainScreen.add(quit);
+	
+		
+	}
+	public void actionPerformed(ActionEvent event) {
+		
+	}
+	public static void main (String[] args) {
+		JFrame frame = new JFrame();
+		MenuBar bar = new MenuBar(new MainScreen(frame));
+		frame.setContentPane(bar);
+		frame.pack();
+		frame.setVisible(true);
+	}
 }
