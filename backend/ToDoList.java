1c2995dc0bd93d9c5d577b7ef8ba51c069f56914
aechen2330 <74388890+aechen2330@users.noreply.github.com>
2020-12-18 11:20:49 -0500

    updateListOrder now accounts for actionItems that have had their priority changed to complete

--- backend/ToDoList.java
+++ backend/ToDoList.java
@@ -29,52 +29,35 @@ public class ToDoList {
 		incompleteItems.add(newIndex, item);
 		if (oldIndex < newIndex) {
 			if (item.getPriority() == Priority.INACTIVE
-					&& incompleteItems.get(newIndex - 1)
-							.getPriority() == Priority.INACTIVE
-					&& item.getActiveByDate() != incompleteItems
-							.get(newIndex - 1).getActiveByDate()) {
-				item.setEventualByDate(
-						incompleteItems.get(newIndex - 1).getActiveByDate());
-				item.updateActionItem(item.getTitle(), item.getPriority(),
-						item.getUrgentByDate(), item.getCurrentByDate(),
-						incompleteItems.get(newIndex - 1).getActiveByDate(),
+					&& incompleteItems.get(newIndex - 1).getPriority() == Priority.INACTIVE
+					&& item.getActiveByDate() != incompleteItems.get(newIndex - 1).getActiveByDate()) {
+				item.setEventualByDate(incompleteItems.get(newIndex - 1).getActiveByDate());
+				item.updateActionItem(item.getTitle(), item.getPriority(), item.getUrgentByDate(),
+						item.getCurrentByDate(), incompleteItems.get(newIndex - 1).getActiveByDate(),
 						item.getComment());
 			}
-			if (item.getPriority() != incompleteItems.get(newIndex - 1)
-					.getPriority()) {
-				if (incompleteItems.get(newIndex - 1)
-						.getPriority() == Priority.INACTIVE) {
+			if (item.getPriority() != incompleteItems.get(newIndex - 1).getPriority()) {
+				if (incompleteItems.get(newIndex - 1).getPriority() == Priority.INACTIVE) {
 
 				}
-				item.updateActionItem(item.getTitle(),
-						incompleteItems.get(newIndex - 1).getPriority(),
-						item.getUrgentByDate(), item.getCurrentByDate(),
-						item.getEventualByDate(), item.getComment());
+				item.updateActionItem(item.getTitle(), incompleteItems.get(newIndex - 1).getPriority(),
+						item.getUrgentByDate(), item.getCurrentByDate(), item.getEventualByDate(), item.getComment());
 			}
 		} else {
 			if (item.getPriority() == Priority.INACTIVE
-					&& incompleteItems.get(newIndex + 1)
-							.getPriority() == Priority.INACTIVE
-					&& item.getActiveByDate() != incompleteItems
-							.get(newIndex + 1).getActiveByDate()) {
-				item.setEventualByDate(
-						incompleteItems.get(newIndex + 1).getActiveByDate());
-				item.updateActionItem(item.getTitle(), item.getPriority(),
-						item.getUrgentByDate(), item.getCurrentByDate(),
-						incompleteItems.get(newIndex + 1).getActiveByDate(),
+					&& incompleteItems.get(newIndex + 1).getPriority() == Priority.INACTIVE
+					&& item.getActiveByDate() != incompleteItems.get(newIndex + 1).getActiveByDate()) {
+				item.setEventualByDate(incompleteItems.get(newIndex + 1).getActiveByDate());
+				item.updateActionItem(item.getTitle(), item.getPriority(), item.getUrgentByDate(),
+						item.getCurrentByDate(), incompleteItems.get(newIndex + 1).getActiveByDate(),
 						item.getComment());
 			}
-			if (item.getPriority() != incompleteItems.get(newIndex + 1)
-					.getPriority()) {
-				if (incompleteItems.get(newIndex + 1)
-						.getPriority() == Priority.INACTIVE) {
-					item.setEventualByDate(incompleteItems.get(newIndex + 1)
-							.getActiveByDate());
+			if (item.getPriority() != incompleteItems.get(newIndex + 1).getPriority()) {
+				if (incompleteItems.get(newIndex + 1).getPriority() == Priority.INACTIVE) {
+					item.setEventualByDate(incompleteItems.get(newIndex + 1).getActiveByDate());
 				}
-				item.updateActionItem(item.getTitle(),
-						incompleteItems.get(newIndex + 1).getPriority(),
-						item.getUrgentByDate(), item.getCurrentByDate(),
-						item.getEventualByDate(), item.getComment());
+				item.updateActionItem(item.getTitle(), incompleteItems.get(newIndex + 1).getPriority(),
+						item.getUrgentByDate(), item.getCurrentByDate(), item.getEventualByDate(), item.getComment());
 			}
 		}
 	}
@@ -91,36 +74,28 @@ public class ToDoList {
 			if (this.incompleteItems.get(i).getPriority() == Priority.URGENT) {
 				urgent.add(this.incompleteItems.get(i));
 			}
-			if (this.incompleteItems.get(i).getPriority() == Priority.CURRENT) {
+			else if (this.incompleteItems.get(i).getPriority() == Priority.CURRENT) {
 				current.add(this.incompleteItems.get(i));
 			}
-			if (this.incompleteItems.get(i)
-					.getPriority() == Priority.EVENTUAL) {
+			else if (this.incompleteItems.get(i).getPriority() == Priority.EVENTUAL) {
 				eventual.add(this.incompleteItems.get(i));
 			}
-			if (this.incompleteItems.get(i)
-					.getPriority() == Priority.INACTIVE) {
+			else if (this.incompleteItems.get(i).getPriority() == Priority.INACTIVE) {
 				unsortedInactive.add(this.incompleteItems.get(i));
+			}
+			else if (this.incompleteItems.get(i).getPriority() == Priority.COMPLETED) {
+				completeActionItem(incompleteItems.get(i));
 			}
 		}
 		while (unsortedInactive.size() > 0) {
 			ActionItem earliest = unsortedInactive.get(0);
 			for (int i = 1; i < unsortedInactive.size(); i++) {
-				if (unsortedInactive.get(i).getActiveByDate()
-						.isBefore(earliest.getActiveByDate()))
+				if (unsortedInactive.get(i).getActiveByDate().isBefore(earliest.getActiveByDate()))
 					earliest = unsortedInactive.get(i);
 			}
 			unsortedInactive.remove(earliest);
 			inactive.add(earliest);
 		}
-		/*
-		 * for (int j = 0; j < unsortedInactive.size(); j++) { ActionItem
-		 * earliest = unsortedInactive.get(0); for (int i = 0; i <
-		 * unsortedInactive.size() - 1; i++) { if
-		 * (unsortedInactive.get(i).getActiveByDate().isBefore(unsortedInactive.
-		 * get(i + 1).getActiveByDate())) { earliest = unsortedInactive.get(i);
-		 * unsortedInactive.remove(i); } } inactive.add(earliest); }
-		 */
 		for (ActionItem item : urgent) {
 			incompleteItems.add(item);
 		}
@@ -139,8 +114,7 @@ public class ToDoList {
 	public void completeActionItem(int index) {
 		ActionItem item = incompleteItems.get(index);
 		item.setCompletedByDate(LocalDateTime.now());
-		item.updateActionItem(item.getTitle(), Priority.COMPLETED,
-				item.getUrgentByDate(), item.getCurrentByDate(),
+		item.updateActionItem(item.getTitle(), Priority.COMPLETED, item.getUrgentByDate(), item.getCurrentByDate(),
 				item.getEventualByDate(), item.getComment());
 		completeItems.add(item);
 		incompleteItems.remove(index);
@@ -148,8 +122,7 @@ public class ToDoList {
 
 	public void completeActionItem(ActionItem item) {
 		item.setCompletedByDate(LocalDateTime.now());
-		item.updateActionItem(item.getTitle(), Priority.COMPLETED,
-				item.getUrgentByDate(), item.getCurrentByDate(),
+		item.updateActionItem(item.getTitle(), Priority.COMPLETED, item.getUrgentByDate(), item.getCurrentByDate(),
 				item.getEventualByDate(), item.getComment());
 		completeItems.add(item);
 		incompleteItems.remove(item);
@@ -165,12 +138,30 @@ public class ToDoList {
 	}
 
 	public void addActionItem(ActionItem item) {
-		// item.setPriority(Priority.URGENT);
 		incompleteItems.add(0, item);
 	}
 
 	// this is for restoring from a backup
 	public void addCompleteActionItem(ActionItem item) {
 		completeItems.add(item);
+	}
+
+	public static void main(String[] arg) {
+		ToDoList list = new ToDoList();
+		for (int i = 0; i < 5; i++) {
+			ActionItem item1 = new ActionItem();
+			item1.setPriority(Priority.URGENT);
+			ActionItem item2 = new ActionItem();
+			item2.setPriority(Priority.CURRENT);
+			ActionItem item3 = new ActionItem();
+			item3.setPriority(Priority.EVENTUAL);
+			list.addActionItem(item1);
+			list.addActionItem(item2);
+			list.addActionItem(item3);
+		}
+		list.updateListOrder();
+		for (int i = 0; i < list.getNumIncompleteItems(); i++) {
+			System.out.println(list.getIncompleteItemAtIndex(i).getPriority());
+		}
 	}
 }
